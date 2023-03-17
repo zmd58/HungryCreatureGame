1f833b0a1a3f80725821b26b863a2cf04a4c39a4
Zoe <eozreality@gmail.com>
2023-03-17 19:59:39 -0400

    finish skateboard

---
+++ hungrycreaturegame/src/Main.java
@@ -0,0 +1,7 @@
+import UI.GameFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
---
+++ hungrycreaturegame/src/UI/GameFrame.java
@@ -0,0 +1,16 @@
+package UI;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    public GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Hungry Creature");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
